77ef948f1db4960b6867233e0e7c20346913f5cd
luismagr <yo@luismagonzalez.es>
2016-03-20 16:01:16 +0100

    CGTX-14 Formulario drenaje Geotexan

---
+++ js/drenaje_g.js
@@ -0,0 +1,140 @@
+var drenajes = {
+	"data1" : {
+		"Terreno" : "cohesivo",
+		"Arido" : "machacado",
+		"Dmax" : "ge0-l60",
+		"Geotexan" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-12"
+	},
+	"data2" : {
+		"Terreno" : "cohesivo",
+		"Arido" : "machacado",
+		"Dmax" : "ge60-l100",
+		"Geotexan" : {"RT" : "7.5", "ELONG" : "50", "RCBR": "1300", "PERF_CONO" : "<35", "AB_PORO" : "<0.07", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-12"
+	},
+	"data3" : {
+		"Terreno" : "cohesivo",
+		"Arido" : "machacado",
+		"Dmax" : "ge100",
+		"Geotexan" : {"RT" : "8", "ELONG" : "50", "RCBR": "1500", "PERF_CONO" : "<35", "AB_PORO" : "<0.065", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-13"
+	},
+	"data4" : {
+		"Terreno" : "cohesivo",
+		"Arido" : "medondeado",
+		"Dmax" : "ge0-l60",
+		"Geotexan" : {"RT" : "5", "ELONG" : "50", "RCBR": "1000", "PERF_CONO" : "<40", "AB_PORO" : "<0.08", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-10"
+	},
+	"data5" : {
+		"Terreno" : "cohesivo",
+		"Arido" : "redondeado",
+		"Dmax" : "ge60-l100",
+		"Geotexan" : {"RT" : "6", "ELONG" : "50", "RCBR": "1100", "PERF_CONO" : "<40", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-11"
+	},
+	"data6" : {
+		"Terreno" : "cohesivo",
+		"Arido" : "redondeado",
+		"Dmax" : "ge100",
+		"Geotexan" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-12"
+	},
+	"data7" : {
+		"Terreno" : "no_cohesivo",
+		"Arido" : "machacado",
+		"Dmax" : "ge0-l60",
+		"Geotexan" : {"RT" : "7.5", "ELONG" : "50", "RCBR": "1300", "PERF_CONO" : "<35", "AB_PORO" : "<0.07", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-12"
+	},
+	"data8" : {
+		"Terreno" : "no_cohesivo",
+		"Arido" : "machacado",
+		"Dmax" : "ge60-l100",
+		"Geotexan" : {"RT" : "6", "ELONG" : "50", "RCBR": "1100", "PERF_CONO" : "<40", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-11"
+	},
+	"data9" : {
+		"Terreno" : "no_cohesivo",
+		"Arido" : "machacado",
+		"Dmax" : "ge100",
+		"Geotexan" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-12"
+	},
+	"data10" : {
+		"Terreno" : "no_cohesivo",
+		"Arido" : "redondeado",
+		"Dmax" : "ge0-l60",
+		"Geotexan" : {"RT" : "7.5", "ELONG" : "50", "RCBR": "1300", "PERF_CONO" : "<35", "AB_PORO" : "<0.07", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-12"
+	},
+	"data11" : {
+		"Terreno" : "no_cohesivo",
+		"Arido" : "redondeado",
+		"Dmax" : "ge60-l100",
+		"Geotexan" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-12"
+	},
+	"data12" : {
+		"Terreno" : "no_cohesivo",
+		"Arido" : "redondeado",
+		"Dmax" : "ge100",
+		"Geotexan" : {"RT" : "7.5", "ELONG" : "50", "RCBR": "1300", "PERF_CONO" : "<35", "AB_PORO" : "<0.07", "KVERT" : ">90"},
+		"Geotextil" : {"RT" : "7", "ELONG" : "50", "RCBR": "1200", "PERF_CONO" : "<35", "AB_PORO" : "<0.075", "KVERT" : ">90"},
+		"GEOTESAN" : "NT-12"
+	}
+};
+
+function calcula() {
+	var terreno = $('#terreno').val();
+	var arido = $('#arido').val();
+	var id = $('#i-d').val();
+
+	if (id < 0) {
+		alert('El valor de D debe ser mayor que 0');
+		return false;
+	} else if (id >= 0 && id < 60 ) {
+		var d = 'ge0-l60';
+	} else if (id >= 60 && id < 100) {
+		var d = 'ge60-l100';
+	} else {
+		var d = 'ge100';
+	}
+
+
+	$.each(drenajes, function(i, item) {
+		if (drenajes[i]['Terreno'] === terreno ) {
+			if (drenajes[i]['Arido'] === arido ) {
+				if (drenajes[i]['Dmax'] == d ) {
+					//Relleno valores del formulario y salgo de la iteración
+					$('#rt-geotexan-value').val(drenajes[i]['Geotexan']['RT']);
+					$('#elong-geotexan-value').val(drenajes[i]['Geotexan']['ELONG']);
+					$('#cbr-geotexan-value').val(drenajes[i]['Geotexan']['RCBR']);
+					$('#rperf-geotexan-value').val(drenajes[i]['Geotexan']['PERF_CONO']);
+					$('#abertura-poro-geotexan-value').val(drenajes[i]['Geotexan']['AB_PORO']);
+					$('#kvert-geotexan-value').val(drenajes[i]['Geotexan']['KVERT']);
+					$('#rt-geotextil-value').val(drenajes[i]['Geotextil']['RT']);
+					$('#elong-geotextil-value').val(drenajes[i]['Geotextil']['ELONG']);
+					$('#cbr-geotextil-value').val(drenajes[i]['Geotextil']['RCBR']);
+					$('#rperf-geotextil-value').val(drenajes[i]['Geotextil']['PERF_CONO']);
+					$('#abertura-poro-geotextil-value').val(drenajes[i]['Geotextil']['AB_PORO']);
+					$('#kvert-geotextil-value').val(drenajes[i]['Geotextil']['KVERT']);
+					$('#product').val(drenajes[i]['GEOTESAN']);
+					return false;
+				}
+			}
+		}
+	});
+}
