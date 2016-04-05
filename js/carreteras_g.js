ee901b91f4a85ce68d7fe40ab42ed970f7e8ceae
luismagr <yo@luismagonzalez.es>
2016-04-05 11:57:11 +0200

    Commit temporal

--- js/carreteras_g.js
+++ js/carreteras_g.js
@@ -1,160 +1,216 @@
-var carreteras = 
-{
-	"data1" : {
-		"CBR": "0-1",
-		"Espesor" : "ge400",
-		"Dmax" : "l150",
-		"Geotexan" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-21"
-		},
-	"data2" : {
-		"CBR": "0-1",
-		"Espesor" : "ge400",
-		"Dmax" : "ge150-l300",
-		"Geotexan" : { "RT": "20", "ELONG" : "50", "RCBR" : "3000", "RPERF_CONO": "15", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "20", "ELONG" : "50", "RCBR" : "3000", "RPERF_CONO": "15", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-25"
-		},
-	"data3" : {
-		"CBR": "0-1",
-		"Espesor" : "ge200-l400",
-		"Dmax" : "l150",
-		"Geotexan" : { "RT": "17", "ELONG" : "50", "RCBR" : "3000", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "17", "ELONG" : "50", "RCBR" : "3000", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-25"
-		},
-	"data4" : {
-		"CBR": "1-3",
-		"Espesor" : "ge400",
-		"Dmax" : "l150",
-		"Geotexan" : { "RT": "9", "ELONG" : "50", "RCBR" : "1500", "RPERF_CONO": "30", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "9", "ELONG" : "50", "RCBR" : "1500", "RPERF_CONO": "30", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-15"
-		},
-	"data5" : {
-		"CBR": "1-3",
-		"Espesor" : "ge400",
-		"Dmax" : "ge150-l300",
-		"Geotexan" : { "RT": "16", "ELONG" : "50", "RCBR" : "2700", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "16", "ELONG" : "50", "RCBR" : "2700", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-23"
-		},
-	"data6" : {
-		"CBR": "1-3",
-		"Espesor" : "ge200-l400",
-		"Dmax" : "ge150-l300",
-		"Geotexan" : { "RT": "17", "ELONG" : "50", "RCBR" : "3000", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "17", "ELONG" : "50", "RCBR" : "3000", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-21"
-		},
-	"data7" : {
-		"CBR": "1-3",
-		"Espesor" : "ge400",
-		"Dmax" : "l150",
-		"Geotexan" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-25"
-		},
-	"data8" : {
-		"CBR": "3-6",
-		"Espesor" : "ge400",
-		"Dmax" : "l150",
-		"Geotexan" : { "RT": "7", "ELONG" : "50", "RCBR" : "1300", "RPERF_CONO": "35", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "7", "ELONG" : "50", "RCBR" : "1300", "RPERF_CONO": "35", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-12"
-		},
-	"data9" : {
-		"CBR": "3-6",
-		"Espesor" : "ge400",
-		"Dmax" : "ge150-l300",
-		"Geotexan" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-21"
-		},
-	"data10" : {
-		"CBR": "3-6",
-		"Espesor" : "ge200-l400",
-		"Dmax" : "l150",
-		"Geotexan" : { "RT": "12", "ELONG" : "50", "RCBR" : "1900", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "12", "ELONG" : "50", "RCBR" : "1900", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-18"
-		},
-	"data11" : {
-		"CBR": "3-6",
-		"Espesor" : "ge200-l400",
-		"Dmax" : "ge150-l300",
-		"Geotexan" : { "RT": "16", "ELONG" : "50", "RCBR" : "2700", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "16", "ELONG" : "50", "RCBR" : "2700", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-23"
-		},
-	"data12" : {
-		"CBR": "ge6",
-		"Espesor" : "ge400",
-		"Dmax" : "l150",
-		"Geotexan" : { "RT": "12", "ELONG" : "50", "RCBR" : "1900", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "12", "ELONG" : "50", "RCBR" : "1900", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-18"
-		},
-	"data13" : {
-		"CBR": "ge6",
-		"Espesor" : "ge400",
-		"Dmax" : "ge150-l300",
-		"Geotexan" : { "RT": "16", "ELONG" : "50", "RCBR" : "2700", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "16", "ELONG" : "50", "RCBR" : "2700", "RPERF_CONO": "20", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-23"
-		},
-	"data14" : {
-		"CBR": "ge6",
-		"Espesor" : "ge200-l400",
-		"Dmax" : "l150",
-		"Geotexan" : { "RT": "10", "ELONG" : "50", "RCBR" : "1700", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "10", "ELONG" : "50", "RCBR" : "1700", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-175"
-		},
-	"data15" : {
-		"CBR": "ge6",
-		"Espesor" : "ge200-l400",
-		"Dmax" : "ge150-l300",
-		"Geotexan" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "25", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-21"
-		},
-	"data16" : {
-		"CBR": "g0",
-		"Espesor" : "ge400-l600",
-		"Dmax" : "300-400",
-		"Geotexan" : { "RT": "21", "ELONG" : "50", "RCBR" : "3900", "RPERF_CONO": "10", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "21", "ELONG" : "50", "RCBR" : "3900", "RPERF_CONO": "10", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-35"
-		},
-	"data17" : {
-		"CBR": "g0",
-		"Espesor" : "ge600-l750",
-		"Dmax" : "400-500",
-		"Geotexan" : { "RT": "25", "ELONG" : "50", "RCBR" : "4400", "RPERF_CONO": "10", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "25", "ELONG" : "50", "RCBR" : "4400", "RPERF_CONO": "10", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-40"
-		},
-	"data18" : {
-		"CBR": "g0",
-		"Espesor" : "ge750-l900",
-		"Dmax" : "500-600",
-		"Geotexan" : { "RT": "27", "ELONG" : "50", "RCBR" : "5200", "RPERF_CONO": "10", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "27", "ELONG" : "50", "RCBR" : "5200", "RPERF_CONO": "10", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-46"
-		},
-	"data19" : {
-		"CBR": "g0",
-		"Espesor" : "ge900",
-		"Dmax" : "ge600",
-		"Geotexan" : { "RT": "31", "ELONG" : "50", "RCBR" : "6400", "RPERF_CONO": "10", "AB_PORO": "0.05-0.2"},
-		"Geotextil" : { "RT": "13", "ELONG" : "50", "RCBR" : "2200", "RPERF_CONO": "0.05-0,2", "AB_PORO": "0.05-0.2"},
-		"GEOTESAN" : "NT-58"
-		},
+var carreteras_gtx = [
+  {
+    "Espesor":"[200..400)",
+    "Dmax":"[0..150)",
+    "CBR":"[0..1)",
+    "Rt":17,
+    "ELONG":50,
+    "RCBR":3000,
+    "RPERF_CONO":20,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-25"
+  },
+  {
+    "Espesor":"[200..400)",
+    "Dmax":"[0..150)",
+    "CBR":"[1..3)",
+    "Rt":13,
+    "ELONG":50,
+    "RCBR":2200,
+    "RPERF_CONO":25,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-21"
+  },
+  {
+    "Espesor":"[200..400)",
+    "Dmax":"[0..150)",
+    "CBR":"[3..6)",
+    "Rt":12,
+    "ELONG":50,
+    "RCBR":1900,
+    "RPERF_CONO":25,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-18"
+  },
+  {
+    "Espesor":"[200..400)",
+    "Dmax":"[0..150)",
+    "CBR":"ge6",
+    "Rt":10,
+    "ELONG":50,
+    "RCBR":1700,
+    "RPERF_CONO":25,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-175"
+  },
+  {
+    "Espesor":"[200..400)",
+    "Dmax":"[150..300)",
+    "CBR":"[1..3)",
+    "Rt":17,
+    "ELONG":50,
+    "RCBR":3000,
+    "RPERF_CONO":20,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-25"
+  },
+  {
+    "Espesor":"[200..400)",
+    "Dmax":"[150..300)",
+    "CBR":"[3..6)",
+    "Rt":16,
+    "ELONG":50,
+    "RCBR":2700,
+    "RPERF_CONO":20,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-23"
+  },
+  {
+    "Espesor":"[200..400)",
+    "Dmax":"[150..300)",
+    "CBR":"ge6",
+    "Rt":13,
+    "ELONG":50,
+    "RCBR":2200,
+    "RPERF_CONO":25,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-21"
+  },
+  {
+    "Espesor":"ge400",
+    "Dmax":"[0..150)",
+    "CBR":"[0..1)",
+    "Rt":13,
+    "ELONG":50,
+    "RCBR":2200,
+    "RPERF_CONO":25,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-21"
+  },
+  {
+    "Espesor":"ge400",
+    "Dmax":"[0..150)",
+    "CBR":"[1..3)",
+    "Rt":9,
+    "ELONG":50,
+    "RCBR":1500,
+    "RPERF_CONO":30,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-15"
+  },
+  {
+    "Espesor":"ge400",
+    "Dmax":"[0..150)",
+    "CBR":"[3..6)",
+    "Rt":7,
+    "ELONG":50,
+    "RCBR":1300,
+    "RPERF_CONO":35,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-12"
+  },
+  {
+    "Espesor":"ge400",
+    "Dmax":"[0..150)",
+    "CBR":"ge6",
+    "Rt":12,
+    "ELONG":50,
+    "RCBR":1900,
+    "RPERF_CONO":25,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-18"
+  },
+  {
+    "Espesor":"ge400",
+    "Dmax":"[150..300)",
+    "CBR":"[0..1)",
+    "Rt":20,
+    "ELONG":50,
+    "RCBR":3000,
+    "RPERF_CONO":15,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-25"
+  },
+  {
+    "Espesor":"ge400",
+    "Dmax":"[150..300)",
+    "CBR":"[1..3)",
+    "Rt":16,
+    "ELONG":50,
+    "RCBR":2700,
+    "RPERF_CONO":20,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-23"
+  },
+  {
+    "Espesor":"ge400",
+    "Dmax":"[150..300)",
+    "CBR":"[3..6)",
+    "Rt":13,
+    "ELONG":50,
+    "RCBR":2200,
+    "RPERF_CONO":25,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-21"
+  },
+  {
+    "Espesor":"ge400",
+    "Dmax":"[150..300)",
+    "CBR":"ge6",
+    "Rt":16,
+    "ELONG":50,
+    "RCBR":2700,
+    "RPERF_CONO":20,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-23"
+  },
+  {
+    "Espesor":"[400..600)",
+    "Dmax":"[300..400)",
+    "CBR":"Z",
+    "Rt":21,
+    "ELONG":50,
+    "RCBR":3900,
+    "RPERF_CONO":10,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-35"
+  },
+  {
+    "Espesor":"[600..750)",
+    "Dmax":"[400..500)",
+    "CBR":"Z",
+    "Rt":25,
+    "ELONG":50,
+    "RCBR":4400,
+    "RPERF_CONO":10,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-40"
+  },
+  {
+    "Espesor":"[750..900)",
+    "Dmax":"[500..600)",
+    "CBR":"Z",
+    "Rt":27,
+    "ELONG":50,
+    "RCBR":5200,
+    "RPERF_CONO":10,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-46"
+  },
+  {
+    "Espesor":"ge900",
+    "Dmax":"ge600",
+    "CBR":"Z",
+    "Rt":31,
+    "ELONG":50,
+    "RCBR":6400,
+    "RPERF_CONO":10,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-58"
+  }
+];
 
-};
-
+	
 function calcula() {
 	//Inputs
 	var icbr = $('#i-cbr').val();
@@ -164,23 +220,26 @@ function calcula() {
 		alert("Debe introducir todos los valores del formulario");
 		return false;
 	}
+
+	if (isNaN(icbr) | isNaN(ie) | isNaN(id)) {
+		alert("Los valores deben ser numéricos");
+	}
 	
 	//Storage interval from the input
 	var cbr;
 	var e;
 	var d;
 
-	if (icbr <= 0) {
-		alert("CBR debe ser mayor que 0");
+	if (icbr < 0) {
+		alert("CBR debe ser mayor o igual a 0");
 		return false;
-	} else if (icbr > 0 ) {
-		cbr = "g0";
-		if (icbr >= 0 && icbr < 1) {
-			cbr = "0-1";
+	} else if (icbr >= 0 ) {
+		if (icbr < 1) {
+			cbr = "[0..1]";
 		} else if (icbr >= 1 && icbr < 3) {
-			cbr = "1-3";
+			cbr = "[1..3)";
 		} else if (icbr>= 3 && icbr < 6) {
-			cbr = "3-6";
+			cbr = "[3..6)";
 		} else if (icbr >= 6) {
 			cbr = "ge6";
 		}
@@ -190,36 +249,56 @@ function calcula() {
 		alert("E debe ser mayor o igual a 200");
 		return false;
 	} else if (ie >=200 && ie < 400) {
-		e = "ge200-l400";
+		e = "[200..400)";
 	} else if (ie >= 400) {
-		e = "ge400";
+		var is_ge400 = true;
+		if (ie <600) {
+			e = "[400..600)";
+		}
+		if (ie >= 600 && ie < 750) {
+			e = "[600..750)";
+		}
+		if (ie >= 750 && ie < 900) {
+			e = "[750..900)";
+		}
+		if (ie >= 900) {
+			e = "ge900";
+		}
 	}
 
-	if (id < 150) {
-		d = "l150";
-	} else if (id >= 150 && id < 300) {
-		d = "ge150-l300";
-	} else if (id >= 300) {
-		alert("D debe ser menor que 300");
+	if (id < 0) {
+		alert("D debe ser mayor que 0");
 		return false;
+	} else if (id >= 0 && id < 150) {
+		d = "[0..150)";
+	} else if (id >= 150 && id < 300) {
+		d = "[150..300)]";
+	} else if (id >= 300 && id <400) {
+		d = "[300..400)";
+	} else if (id >=400 && id < 500){
+		d = "[400..500)";
+	} else if (id >= 500 && id < 600) {
+		d = "[500..600)";
+	} else if (id >= 600) {
+		d = "ge600";
 	}
 
-	$.each(carreteras, function(i, item) {
-		if (carreteras[i]['CBR'] == cbr ) {
-			if (carreteras[i]['Espesor'] == e) {
-				if (carreteras[i]['Dmax'] == d) {
+	$.each(carreteras_gtx, function(i, item) {
+		if (carreteras_gtx[i]['CBR'] == cbr ) {
+			if (carreteras_gtx[i]['Espesor'] == e) {
+				if (carreteras_gtx[i]['Dmax'] == d) {
 					//Relleno valores del formulario y salgo de la iteración
-					$('#rt-geotexan-value').val(carreteras[i]['Geotexan']['RT']);
-					$('#elong-geotexan-value').val(carreteras[i]['Geotexan']['ELONG']);
-					$('#cbr-geotexan-value').val(carreteras[i]['Geotexan']['RCBR']);
-					$('#perfcono-geotexan-value').val(carreteras[i]['Geotexan']['RPERF_CONO']);
-					$('#abcono-geotexan-value').val(carreteras[i]['Geotexan']['AB_PORO']);
-					$('#rt-geotextil-value').val(carreteras[i]['Geotextil']['RT']);
-					$('#elong-geotextil-value').val(carreteras[i]['Geotextil']['ELONG']);
-					$('#cbr-geotextil-value').val(carreteras[i]['Geotextil']['RCBR']);
-					$('#perfcono-geotextil-value').val(carreteras[i]['Geotextil']['RPERF_CONO']);
-					$('#abcono-geotextil-value').val(carreteras[i]['Geotextil']['AB_PORO']);
-					$('#product').val(carreteras[i]['GEOTESAN']);
+					$('#rt-geotexan-value').val(carreteras_gtx[i]['RT']);
+					$('#elong-geotexan-value').val(carreteras_gtx[i]['ELONG']);
+					$('#cbr-geotexan-value').val(carreteras_gtx[i]['RCBR']);
+					$('#perfcono-geotexan-value').val(carreteras_gtx[i]['RPERF_CONO']);
+					$('#abcono-geotexan-value').val(carreteras_gtx[i]['AB_PORO']);
+					$('#rt-geotextil-value').val(carreteras_gtx[i]['RT']);
+					$('#elong-geotextil-value').val(carreteras_gtx[i]['ELONG']);
+					$('#cbr-geotextil-value').val(carreteras_gtx[i]['RCBR']);
+					$('#perfcono-geotextil-value').val(carreteras_gtx[i]['RPERF_CONO']);
+					$('#abcono-geotextil-value').val(carreteras_gtx[i]['AB_PORO']);
+					$('#product').val(carreteras_gtx[i]['GEOTESAN']);
 					return true;
 				}
 			}
