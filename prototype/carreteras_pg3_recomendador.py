5c41952e371a62724bf192e71688c957c0dbbbbe
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2016-04-06 16:31:39 +0200

    Carreteras usando prototipo (copypaste de muros solo modificando 3 líneas. Works like a charm!

---
+++ prototype/carreteras_pg3_recomendador.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+NUMENTRADAS = 2
+NUMSALIDAS = 5
+CABECERA = (u'Tr\xe1fico (desplegable)', u'Funci\xf3n (desplegable)', u'E', u'Rt', u'R perf. Cono', u'Abertura poro', u'Producto recomendado')
+TABLA = [(u'T0', u'Separaci\xf3n', u'8,92', u'16', u'18,65', u'0,06', u'NT-23'), (u'T0', u'Filtraci\xf3n', u'5,2', u'9,16', u'25,28', u'0,061', u'NT-17'), (u'T1', u'Separaci\xf3n', u'6,44', u'12', u'21,93', u'0,061', u'NT-18'), (u'T1', u'Filtraci\xf3n', u'3,98', u'7,12', u'34,04', u'0,068', u'NT-12'), (u'T2', u'Separaci\xf3n', u'4,58', u'8', u'28,66', u'0,061', u'NT-15'), (u'T2', u'Filtraci\xf3n', u'2,96', u'5,36', u'39', u'0,075', u'NT-10'), (u'T3', u'Separaci\xf3n', u'3,98', u'7,12', u'34,04', u'0,068', u'NT-12'), (u'T3', u'Filtraci\xf3n', u'2,96', u'5,36', u'39', u'0,075', u'NT-10')]
