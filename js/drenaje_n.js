e25d6ef1d0b9689a7ba47eb0585e9236d56387c1
luismagr <yo@luismagonzalez.es>
2016-04-06 16:45:03 +0200

    CGTX-66 Datos de drenaje

--- js/drenaje_n.js
+++ js/drenaje_n.js
@@ -1,30 +1,72 @@
-var drenajes = {
-	"data1" : {
-		"Trafico" : "T0",
-		"Normativa" : { "E" : "2.7", "RT" : "9", "PERF_CONO" : "30", "AB_PORO" : "0.5-0.2"},
-		"Geotexan" : { "E" : "2.7", "RT" : "9", "PERF_CONO" : "30", "AB_PORO" : "0.5-0.2"},
-		"GEOTESAN" : "NT-15"
-	},
-	"data2" : {
-		"Trafico" : "T1",
-		"Normativa" : { "E" : "2.1", "RT" : "7", "PERF_CONO" : "35", "AB_PORO" : "0.5-0.2"},
-		"Geotexan" : { "E" : "2.1", "RT" : "7", "PERF_CONO" : "35", "AB_PORO" : "0.5-0.2"},
-		"GEOTESAN" : "NT-12"
-	},
-	"data3" : {
-		"Trafico" : "T2",
-		"Normativa" : { "E" : "1.5", "RT" : "5", "PERF_CONO" : "40", "AB_PORO" : "0.5-0.2"},
-		"Geotexan" : { "E" : "1.5", "RT" : "5", "PERF_CONO" : "40", "AB_PORO" : "0.5-0.2"},
-		"GEOTESAN" : "NT-10"
-	},
-	"data4" : {
-		"Trafico" : "T3",
-		"Normativa" : { "E" : "1.2", "RT" : "4", "PERF_CONO" : "45", "AB_PORO" : "0.5-0.2"},
-		"Geotexan" : { "E" : "1.2", "RT" : "4", "PERF_CONO" : "45", "AB_PORO" : "0.5-0.2"},
-		"GEOTESAN" : "NT-10"
-	}
+var drenajes_n = [
+  {
+    "Trafico":"T0",
+    "E":"2,7",
+    "RT":9,
+    "RPERF_CONO":30,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-15"
+  },
+  {
+    "Trafico":"T1",
+    "E":"2,1",
+    "RT":7,
+    "RPERF_CONO":35,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-12"
+  },
+  {
+    "Trafico":"T2",
+    "E":"1,5",
+    "RT":5,
+    "RPERF_CONO":40,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-10"
+  },
+  {
+    "Trafico":"T3",
+    "E":"1,2",
+    "RT":4,
+    "RPERF_CONO":45,
+    "AB_PORO":"0,05 – 0,2",
+    "GEOTESAN":"NT-10"
+  }
+];
 
-};
+var drenajes_g  = [
+  {
+    "Trafico":"T0",
+    "E":"57,23",
+    "RT":"9,5",
+    "RPERF_CONO":"28,66",
+    "AB_PORO":"0,061",
+    "GEOTESAN":"NT-15"
+  },
+  {
+    "Trafico":"T1",
+    "E":"55,87",
+    "RT":"8",
+    "RPERF_CONO":"34,04",
+    "AB_PORO":"0,068",
+    "GEOTESAN":"NT-12"
+  },
+  {
+    "Trafico":"T2",
+    "E":"55,19",
+    "RT":"6",
+    "RPERF_CONO":"39",
+    "AB_PORO":"0,075",
+    "GEOTESAN":"NT-10"
+  },
+  {
+    "Trafico":"T3",
+    "E":"55,19",
+    "RT":"6",
+    "RPERF_CONO":"39",
+    "AB_PORO":"0,075",
+    "GEOTESAN":"NT-10"
+  }
+];
 
 var inputs = 
 {
@@ -37,18 +79,18 @@ $(document).ready(function() {
 
 function calcula() {
 	var trafico = $('#trafico').val();
-	$.each(drenajes, function(i, item) {
-		if (drenajes[i]['Trafico'] == trafico ) {
+	$.each(drenajes_n, function(i, item) {
+		if (drenajes_n[i]['Trafico'] == trafico ) {
 			//Relleno valores del formulario y salgo de la iteración
-			$('#e-normativa-value').val(drenajes[i]['Normativa']['E']);
-			$('#rt-normativa-value').val(drenajes[i]['Normativa']['RT']);
-			$('#rperf-normativa-value').val(drenajes[i]['Normativa']['PERF_CONO']);
-			$('#abertura-poro-normativa-value').val(drenajes[i]['Normativa']['AB_PORO']);
-			$('#e-geotextil-value').val(drenajes[i]['Geotexan']['E']);
-			$('#rt-geotextil-value').val(drenajes[i]['Geotexan']['RT']);
-			$('#rperf-geotextil-value').val(drenajes[i]['Geotexan']['PERF_CONO']);
-			$('#abertura-poro-geotextil-value').val(drenajes[i]['Geotexan']['AB_PORO']);
-			$('#product').val(drenajes[i]['GEOTESAN']);
+			$('#e-normativa-value').val(drenajes_n[i]['E']);
+			$('#rt-normativa-value').val(drenajes_n[i]['RT']);
+			$('#rperf-normativa-value').val(drenajes_n[i]['RPERF_CONO']);
+			$('#abertura-poro-normativa-value').val(drenajes_n[i]['AB_PORO']);
+			$('#e-geotexan-value').val(drenajes_g[i]['E']);
+			$('#rt-geotexan-value').val(drenajes_g[i]['RT']);
+			$('#rperf-geotexan-value').val(drenajes_g[i]['RPERF_CONO']);
+			$('#abertura-poro-geotexan-value').val(drenajes_g[i]['AB_PORO']);
+			$('#product').val(drenajes_g[i]['GEOTESAN']);
 			return true;
 		}
 	});
