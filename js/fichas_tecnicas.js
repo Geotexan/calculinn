a101c7b771b85b5869173e48a96effbef4d7560c
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2016-04-07 21:37:41 +0200

    Agregados Geotexgrid a fichas_tecnicas.js.

--- js/fichas_tecnicas.js
+++ js/fichas_tecnicas.js
@@ -16,7 +16,22 @@ var fichas =
 	"NT-35" : "1-1-14_geotesan_nt35.pdf",
 	"NT-40" : "1-1-15_geotesan_nt40.pdf",
 	"NT-46" : "1-1-16_geotesan_nt46.pdf",
-	"NT-58" : "1-1-17_geotesan_nt58.pdf"
+	"NT-58" : "1-1-17_geotesan_nt58.pdf",
+    "GEOTEXGRID 20/20": "1-4-12_geotexgrid-2020_3030_4020_4040_5020.pdf",
+    "GEOTEXGRID 30/30": "1-4-12_geotexgrid-2020_3030_4020_4040_5020.pdf",
+    "GEOTEXGRID 40/20": "1-4-12_geotexgrid-2020_3030_4020_4040_5020.pdf",
+    "GEOTEXGRID 40/40": "1-4-12_geotexgrid-2020_3030_4020_4040_5020.pdf",
+    "GEOTEXGRID 50/20": "1-4-12_geotexgrid-2020_3030_4020_4040_5020.pdf",
+    "GEOTEXGRID 50/60": "1-4-13_geotexgrid-5050_6030_6060_8030_8080.pdf",
+    "GEOTEXGRID 60/30": "1-4-13_geotexgrid-5050_6030_6060_8030_8080.pdf",
+    "GEOTEXGRID 60/60": "1-4-13_geotexgrid-5050_6030_6060_8030_8080.pdf",
+    "GEOTEXGRID 80/30": "1-4-13_geotexgrid-5050_6030_6060_8030_8080.pdf",
+    "GEOTEXGRID 80/80": "1-4-13_geotexgrid-5050_6030_6060_8030_8080.pdf",
+    "GEOTEXGRID 90/90": "1-4-14_geotexgrid-9090_100100_11030_13030_15030.pdf",
+    "GEOTEXGRID 100/100": "1-4-14_geotexgrid-9090_100100_11030_13030_15030.pdf",
+    "GEOTEXGRID 110/30": "1-4-14_geotexgrid-9090_100100_11030_13030_15030.pdf",
+    "GEOTEXGRID 130/30": "1-4-14_geotexgrid-9090_100100_11030_13030_15030.pdf",
+    "GEOTEXGRID 150/30": "1-4-14_geotexgrid-9090_100100_11030_13030_15030.pdf"
 };
 
 /*
@@ -25,10 +40,24 @@ var fichas =
 */
 $(document).ready(function() {
 	$('#pdf').click(function() {
+        var flag_version = 1;   // No se me ocurre otra manera más rápida.
 		var nt = $('#product').val();
-        console.log(nt)
+        if ( nt == ''){
+            // No es un input, viene del template. Es un span.
+            nt = $('#product').text();
+            // console.log("nt");
+            // console.log(nt);
+            /* TODO: Tal y como está ahora, solo mostrará la ficha técnica
+             * del primero de los productos recomendados. Todos los demás
+             * que se agreguen a la tabla llevan el mismo id.
+             */
+            flag_version = 2;
+        }
 		if (typeof fichas[nt] !== 'undefined') {
-			var file = '../pdf/' + fichas[nt];
+    		var file = 'pdf/' + fichas[nt];
+            if (flag_version >= 2){
+                file = '../pdf/' + fichas[nt];
+            }
 			var win = window.open(file, '_blank');
 			if(win){
 			    //Browser has allowed it to be opened
