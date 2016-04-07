bea192da5fea3cda1b56184906ea8356c2f4dcb9
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2016-04-07 15:17:06 +0200

    Agregados formularios generados por prototipo.

---
+++ calculos/drenaje_normativa_recomendador.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+NUMENTRADAS = 2
+NUMSALIDAS = 5
+CABECERA = (u'Funci\xf3n', u'Tr\xe1fico (desplegable)', u'E', u'Rt', u'R perf. Cono', u'Abertura poro', u'Producto recomendado')
+TABLA = [(u'Filtraci\xf3n', u'T0', u'57,23', u'9,5', u'28,66', u'0,061', u'NT 15'), (u'Filtraci\xf3n', u'T1', u'55,87', u'8', u'34,04', u'0,068', u'NT 12'), (u'Filtraci\xf3n', u'T2', u'55,19', u'6', u'39', u'0,075', u'NT 10'), (u'Filtraci\xf3n', u'T3', u'55,19', u'6', u'39', u'0,075', u'NT 10')]
+RANGOS = {u'Funci\xf3n': (u'Filtraci\xf3n',), u'Tr\xe1fico (desplegable)': (u'T0', u'T1', u'T2', u'T3')}
