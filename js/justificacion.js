99a2bf972ccc0bb85998b877f62db1013808872f
Francisco José Rodríguez Bogado <bogado@qinn.es>
2017-09-06 21:13:16 +0200

    Proof of concept de justificación del cálculo.

--- js/justificacion.js
+++ js/justificacion.js
@@ -7,6 +7,13 @@ $(document).ready(function() {
         var url = window.location.href;
         var basename = url.split("/").pop();
         var desturl = url.replace(basename, basename.replace("#", "")).replace("/calculos/", "/just/");
-        console.log(desturl);
+        var params = ["pg3_elon", "gtx_elon", "pg3_trac", "gtx_trac", "pg3_cono", "gtx_cono", "pg3_poro", "gtx_poro", "gtx_nt"];
+        var params_values = [];
+        for (var param in params){
+        	params_values.push(params[param] + "=" + param);
+        }
+        desturl += "?" + params_values.join("&");
+        //console.log(desturl);
+        window.location.href = desturl;
     });
 });
