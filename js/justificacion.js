f388d9c5c7270ae3f1f4e5429bdf0cd7edae7588
Francisco José Rodríguez Bogado <bogado@qinn.es>
2017-09-07 13:59:24 +0200

    Justificación del cálculo completada.

--- js/justificacion.js
+++ js/justificacion.js
@@ -1,18 +1,33 @@
 /*
-* Determina el cálculo solicitado y abre el HTML de justificación del cálculo
-* con los valores obtenidos y el producto recomendado.
-*/
+ * Determina el cálculo solicitado y abre el HTML de justificación del cálculo
+ * con los valores obtenidos y el producto recomendado.
+ */
 $(document).ready(function() {
     $('#just').click(function() {
         var url = window.location.href;
         var basename = url.split("/").pop();
         var desturl = url.replace(basename, basename.replace("#", "")).replace("/calculos/", "/just/");
-        // TODO: En vez de HARDODED, recorrer el DOM y rescatar todo lo que empiece por ref_, gtx_ o prouct.
-        var params = ["ref_E", "gtx_E", "ref_Rt", "gtx_Rt", "ref_R_perf__Cono", "gtx_R_perf__Cono", "ref_Abertura_poro", "gtx_Abertura_poro", "product"];
-        var params_values = [];
-        for (var param in params){
-        	// TODO: En lugar del índice numérico param, el valor del span que está contenido en el div.
-        	var valor = param;
+        // Selector múltiple por expresión regular. Lista de ids a pasar.
+        var selectors = $("div[id^='ref_'],div[id^='gtx_'],div[id='product'],input[id^='in'],select[id^='in']");
+        var params = selectors.map(function() {
+        	return this.id;
+		}).get();
+		// Lista de "valores" de esos ids.
+		var params_values = [];
+		for (var param in params){
+			var selector = '#'.concat(params[param]);
+			// Primero lo intento como si fuera un desplegable:
+        	var valor = $(selector.concat(" option:selected")).text();
+        	// Porque en un desplegable el valor .text() da cosas raras.
+        	// Después ya lo intento como campo de texto normal y como numérico.
+        	if (! valor){
+        		valor = $(selector).text();
+        		//console.log(valor);
+        	}
+        	if (! valor){
+        		valor = $(selector).val();
+        		//console.log(valor);
+        	}
         	params_values.push(params[param] + "=" + valor);
         }
         desturl += "?" + params_values.join("&");
---
+++ js/params_just.js
@@ -0,0 +1,21 @@
+/*
+ * "Parsea" los parámetros de la url y sistutuye los `span` que se llamen 
+ * igual por el valor de cada parámetro.
+ */
+
+var DEBUG=true;
+
+// "Parser" de parámetros. Los guarda en una `hashmap` o como se llamen en JS.
+var params={};location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(s,k,v){params[k]=v})
+
+// Cuando se acaba de cargar la página, hago la sustitución.
+$(document).ready(function() {
+    for (var param  in params) {
+    	var valor=decodeURIComponent(params[param]);
+        $("#"+param).text(valor);
+        if (DEBUG){
+        	console.log(param);
+        	console.log(valor);
+        }
+    }
+});
